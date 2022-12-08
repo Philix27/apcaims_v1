0684d14f99f799eb6618cc37f465319882e92afb
Philix27 <philixbob@gmail.com>
2022-12-08 10:23:36 +0100

    updated all states page

--- pages/dashboard/all_states.js
+++ pages/dashboard/all_states.js
@@ -1,81 +1,95 @@
 import axios from "axios";
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Chart from "../../comps/chart";
 import { bgColors } from "../../constants/chartColors";
 import { data } from "../../constants/states/";
+import { utils } from "../../utils";
+import { Circles } from "react-loader-spinner";
 
-export default function SummaryPage({ agentsList }) {
+function getCount(fGentList, state) {
+  console.log(state, fGentList);
+
+  if (fGentList.data.length > 1) {
+    let ags = fGentList.data.filter((ag) => ag.state == state);
+    return ags.length;
+  } else {
+    return 0;
+  }
+}
+
+const label = data.states.map((val) => val.state);
+const agentCount = (gentsList) =>
+  data.states.map((val) => getCount(gentsList, val.state));
+
+export default function AllStatesPage({}) {
+  //   let agentsList;
   const router = useRouter();
   const query = router.query;
   const userState = query.state;
+  const [loading, setLoading] = useState(true);
+  const [gentsList, setGentsList] = useState([]);
 
-  //   const selectedLg = data.states.filter((val) =>
-  //     val.state.toLowerCase().includes(userState.toLowerCase())
-  //   );
-  //   console.log("selectedLg");
-  //   console.log(selectedLg);
+  useEffect(async () => {
+    try {
+      let art;
+      art = await axios(`${process.env.NEXT_PUBLIC_DOMAIN}/api/all_states`);
+      setGentsList(art.data);
+      setLoading(false);
+      console.log("completed fetching");
+    } catch (e) {}
+  }, []);
 
-  function getCount(state) {
-    let ags = agentsList.data.filter((ag) => ag.state == state);
-    return ags.length;
-  }
-
-  const label = data.states.map((val, index) => val.state);
-  const agentCount = data.states.map((val) => getCount(val.state));
-  //   const agentCount = [1, 2, 4, 6, 7, 8, 55, 3, 12, 5, 34, 98];
-
-  const [userData, setUserData] = useState({
-    labels: label,
-    datasets: [
-      {
-        label: "LGAs",
-        data: agentCount,
-        minBarLength: 10,
-        backgroundColor: bgColors,
-        xAxisID: "States",
-        yAxisID: "Agent count",
-        scaleShowValues: true,
-        scales: {
-          x: {
-            ticks: {
-              autoSkip: false,
-              maxRotation: 90,
-              minRotation: 90,
+  if (loading) {
+    return (
+      <div className="section">
+        <h1>Fetching Data...</h1>
+        <div className="loader">
+          <Circles
+            height="200"
+            width="200"
+            color="#1663b0"
+            ariaLabel="circles-loading"
+            visible={true}
+          />
+        </div>
+      </div>
+    );
+  } else {
+    const userData = {
+      labels: label,
+      datasets: [
+        {
+          label: "Agents",
+          data: agentCount(gentsList),
+          minBarLength: 10,
+          backgroundColor: bgColors,
+          xAxisID: "States",
+          yAxisID: "Agent count",
+          scaleShowValues: true,
+          scales: {
+            x: {
+              ticks: {
+                autoSkip: false,
+                maxRotation: 90,
+                minRotation: 90,
+              },
             },
           },
         },
-      },
-    ],
-  });
-
-  return (
-    <div>
-      <Chart
-        chartdata={userData}
-        title="Agents in the various LGAs"
-        chartType="DOUGHNUT"
-      />
-    </div>
-  );
-}
-
-export async function getServerSideProps(context) {
-  try {
-    let art;
-    art = await axios(`${process.env.NEXT_PUBLIC_DOMAIN}/api/all_states`);
-    return {
-      props: {
-        agentsList: art.data,
-      },
+      ],
     };
-  } catch (error) {
-    console.log("Opps an error");
-    return {
-      props: {
-        agentsList: [],
-      },
-    };
+
+    return (
+      <div>
+        <h1>Agent's Count</h1>
+
+        <Chart
+          chartdata={userData}
+          title={`${utils.numberWithCommas(gentsList.length)} Agents`}
+          chartType="DOUGHNUT"
+        />
+      </div>
+    );
   }
-  // console.log(art);
 }
