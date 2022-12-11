a82e7f9ddbb1f0622cb1142c04212a48f763e3a9
Philix27 <philixbob@gmail.com>
2022-12-11 17:58:55 +0100

    admin all states

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,14 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import Link from "next/link";
-import axios from "axios";
 import { data } from "../../constants/states";
-import { utils } from "../../utils";
-import { MdPersonAddAlt1, MdGroups } from "react-icons/md";
-import { GoGraph } from "react-icons/go";
-import { bgColors } from "../../constants/chartColors";
-import Chart from "../../comps/chart_bar";
 
 export default function Dashboard({ agentsList, stateValue }) {
   const router = useRouter();
@@ -26,13 +20,13 @@ export default function Dashboard({ agentsList, stateValue }) {
         <h1>All States</h1>
       </div>
       <div className="dashboard">
-        {data.states.map((val) => (
-          <Link passHref href={`/dashboard/${val.state}`}>
+        {data.states.map((val, i) => (
+          <Link passHref href={`/dashboard/${val.state}`} key={i}>
             <div className="card">
               <div className="topbar teal"></div>
               <div className="content">
                 <h3>{val.state}</h3>
-                <p>All state's info</p>
+                <p>Informations and details for {val.state} state.</p>
               </div>
             </div>
           </Link>
