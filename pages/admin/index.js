fd1b40c47135242a97924ad3df4635f1bde097b6
Philix27 <philixbob@gmail.com>
2022-12-12 09:52:17 +0100

    graph for all states

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import Link from "next/link";
@@ -6,15 +6,15 @@ import { data } from "../../constants/states";
 import axios from "axios";
 import { utils } from "../../utils";
 import { parse } from "uuid";
+import Chart from "../../comps/chart";
+import { bgColors } from "../../constants/chartColors";
 
 export default function Dashboard({ statesCount }) {
   const router = useRouter();
   const query = router.query;
   const userState = query.state;
-  // console.log(statesCount);
 
   function stateAgentCount(nameOfState) {
-    console.log(nameOfState);
     let counterValue = statesCount.data.map((st) => {
       if (st._id.toUpperCase() == nameOfState.toUpperCase()) {
         return parseInt(st.agentCount);
@@ -23,14 +23,30 @@ export default function Dashboard({ statesCount }) {
 
     return counterValue;
   }
-  // function stateAgentCount(nameOfState) {
-  //   console.log(nameOfState);
-  //   let ags = statesCount.data.filter(
-  //     (st) => st._id.toUpperCase() == nameOfState.toUpperCase()
-  //   );
 
-  //   return ags.length;
-  // }
+  const label = data.states.map((val, i) => val.state);
+
+  function dataSets() {
+    let vv = label.map((v) => {
+      var cc = statesCount.data.filter(
+        (st) => st._id.toUpperCase() == v.toUpperCase()
+      );
+      return cc.agentCount;
+    });
+
+    return vv;
+  }
+
+  const [userData, setUserData] = useState({
+    labels: statesCount.data.map((val) => val._id),
+    datasets: [
+      {
+        label: "LGAs",
+        data: statesCount.data.map((val) => val.agentCount),
+        backgroundColor: bgColors,
+      },
+    ],
+  });
 
   return (
     <div className="dasboardWrapper">
@@ -42,7 +58,13 @@ export default function Dashboard({ statesCount }) {
       <div className="headingSection">
         <h1>All States</h1>
       </div>
-
+      <div className="headingSection">
+        <a className="btn" onClick={() => getAgentsCounts()}>
+          Run
+        </a>
+      </div>
+      <Chart chartdata={userData} title="Agents in all states" />
+      {/* <Chart chartdata={userData} title="Agents by " chartType="DOUGHNUT" /> */}
       <div className="dashboard">
         {data.states.map((val, i) => (
           <Link passHref href={`/dashboard/${val.state}`} key={i}>
@@ -52,7 +74,7 @@ export default function Dashboard({ statesCount }) {
                 <h3>{val.state}</h3>
                 <p>Informations and details for state.</p>
                 {/* <p>{utils.numberWithCommas(stateAgentCount(val.state))}</p> */}
-                <p>{stateAgentCount(val.state)}</p>
+                <p>{stateAgentCount(val.state)} agents</p>
               </div>
             </div>
           </Link>
