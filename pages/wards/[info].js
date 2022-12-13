231b7aa0ada24712cdbd518bc683e22aa9339bef
Philix27 <philixbob@gmail.com>
2022-12-13 18:59:49 +0100

    agent count by wards

--- pages/wards/[info].js
+++ pages/wards/[info].js
@@ -1,13 +1,19 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { data } from "../../constants/states/index";
+import axios from "axios";
 
-export default function LGAs() {
+export default function Wards({ agentsList }) {
   const router = useRouter();
   let { info } = router.query;
-
+  console.log(agentsList);
   const getStatesInfo = () =>
     data.states.filter((state) => state.state == info);
+
+  function getWardsCount(lga, ward) {
+    const fl = agentsList.data.filter((ag) => ag.lga == lga && ag.ward == ward);
+    return fl.length;
+  }
 
   return (
     <div className="wards">
@@ -18,11 +24,13 @@ export default function LGAs() {
               <div className="info" key={lgaIndex}>
                 <div className="tile">
                   <h4>{`${lgaIndex + 1}. ${lga.name}`}</h4>
+                  <p>agents</p>
                 </div>
                 <div className="moreInfo">
                   {lga.wards.map((ward, wardInex) => (
                     <div className="moreInfoList" key={wardInex}>
                       <p>{ward}</p>
+                      <p>{getWardsCount(lga.name, ward)}</p>
                     </div>
                   ))}
                 </div>
@@ -33,4 +41,21 @@ export default function LGAs() {
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { query } = context;
+  console.log(query);
+
+  let art;
+  art = await axios(
+    `${process.env.NEXT_PUBLIC_DOMAIN}/api/lga_wards?state=${query.info}`
+  );
+
+  // console.log(art);
+  return {
+    props: {
+      agentsList: art.data,
+    },
+  };
 }
