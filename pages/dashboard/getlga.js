3b65443a3781f76624debb552b8f3bdd83e91b9b
Philix27 <philixbob@gmail.com>
2023-01-04 18:23:04 +0100

    added wards to getlga views

--- pages/dashboard/getlga.js
+++ pages/dashboard/getlga.js
@@ -5,6 +5,8 @@ import { data } from "../../constants/states/index";
 
 export default function AddLga() {
   const [localGovs, setLocalGov] = useState([]);
+  const [wards, setWards] = useState([]);
+  const [selectedLga, setSelectedLga] = useState("");
 
   const handleChange = (e) => {
     const selectedState = e.target.value;
@@ -14,42 +16,72 @@ export default function AddLga() {
     setLocalGov(selection[0].lga);
   };
 
+  const handleOnLgaClick = (lga) => {
+    setSelectedLga(lga);
+    const selectedLocalGov = localGovs.filter((_val) => _val.name == lga);
+    setWards(selectedLocalGov[0].wards);
+  };
+
   return (
-    <div className="section addLga">
-      <h3>Get LGAs</h3>
+    <div className="lgaWardInfo">
+      <div className="item">
+        <h3>Get LGAs</h3>
+        <form className="form">
+          <div className="input_box">
+            <label htmlFor="form-state">State</label>
+            <select
+              name="state"
+              onChange={handleChange}
+              // value={agent.state}
+              required
+              id="form-state"
+            >
+              <option disabled>Select a state</option>
 
-      <form className="form">
-        <div className="input_box">
-          <label htmlFor="form-state">State</label>
-          <select
-            name="state"
-            onChange={handleChange}
-            // value={agent.state}
-            required
-            id="form-state"
-          >
-            <option disabled>Select a state</option>
-
-            {data.states.map((_val, index) => {
-              return (
-                <option value={_val.state.toUpperCase()} key={index}>
-                  {_val.state}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-      </form>
-      <div className="lgaList">
-        <h3>List</h3>
-        {localGovs.map((lga, index) => (
-          <div key={index} className="listItem">
-            {index + 1}. <span className="lgaTitle">{lga.name}</span>
-            <CopyToClipboard text={lga.name.trim()}>
-              <button>Copy</button>
-            </CopyToClipboard>
+              {data.states.map((_val, index) => {
+                return (
+                  <option value={_val.state.toUpperCase()} key={index}>
+                    {_val.state}
+                  </option>
+                );
+              })}
+            </select>
           </div>
-        ))}
+        </form>
+        <div className="lgaList">
+          <h3>List of LGAs</h3>
+          {localGovs.map((lga, index) => (
+            <div
+              key={index}
+              className={
+                selectedLga == lga.name ? "selectedListItem" : "listItem"
+              }
+            >
+              {index + 1}. <span className="lgaTitle">{lga.name}</span>
+              <CopyToClipboard
+                text={lga.name.trim()}
+                // onClick={() => handleOnLgaClick(lga.name)}
+              >
+                <button onClick={() => handleOnLgaClick(lga.name)}>Copy</button>
+              </CopyToClipboard>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="item">
+        <div>
+          <div className="lgaList">
+            <h3>Wards for {selectedLga}</h3>
+            {wards.map((ward, index) => (
+              <div key={index} className="listItem">
+                {index + 1}. <span className="lgaTitle">{ward}</span>
+                <CopyToClipboard text={ward.trim()}>
+                  <button>Copy</button>
+                </CopyToClipboard>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
